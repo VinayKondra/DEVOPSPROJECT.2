da8283f20ad33750ffaf3979cfa46d50fdd24583
Vinay Kondra <kondravinay5@gmail.com>
2025-11-14 21:31:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("learnMoreBtn").addEventListener("click", function() {
+  alert("DevOps combines development and operations to improve software delivery speed and reliability!");
+});
